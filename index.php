119cce4b314db45387b333aec9e0e03ebbe4a055
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-11-17 23:07:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Permutations</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <h1>Given positive integers n and r, find the number of r-permutations when repetition is allowed of a set with n elements.</h1>
+    <form method="post">
+      <h2>Enter the Numbers:</h2><br>
+      <label for="firstNumber">Number of Objects (n):</label><br>
+      <input type="number" name="firstNumber" id="firstNumber"><br>
+      <label for="secondNumber">Number of Samples (r):</label><br>
+      <input type="number" name="secondNumber" id="secondNumber"><br>
+      <input type="submit" name="submit" value="Solve" />
+    </form>
+    <h3>
+      <?php
+      if(isset($_POST['firstNumber']) && isset($_POST['secondNumber'])){
+        $n = $_POST['firstNumber'];
+        $r = $_POST['secondNumber'];
+      }
+        if($_POST) {
+          if ($n > "0" && $r > "0") {
+            echo(pow($n,$r));
+          }
+          else{
+            echo "Integer(s) must be positive";
+          }
+        }
+      ?>
+    </h3>
+  </body>
+</html>
