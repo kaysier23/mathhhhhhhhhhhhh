0a96d066078ffde7872fd2c5a7448250aff7a366
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-11-18 09:52:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
   </head>
   <body>
     <h1>Given positive integers n and r, find the number of r-permutations when repetition is allowed of a set with n elements.</h1>
+<h2>Enter the Numbers:</h2><br>
     <form method="post">
-      <h2>Enter the Numbers:</h2><br>
       <label for="firstNumber">Number of Objects (n):</label><br>
       <input type="number" name="firstNumber" id="firstNumber"><br>
       <label for="secondNumber">Number of Samples (r):</label><br>
@@ -32,3 +32,4 @@
     </h3>
   </body>
 </html>
+
